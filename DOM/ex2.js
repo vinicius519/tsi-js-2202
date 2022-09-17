7ab5f9193353ab97ca68b190607d0d2538c3869f
VINICIUS GABRIEL FERNANDES DA SILVA <vinicius.gfsilva1@senacsp.edu.br>
2022-09-16 22:10:55 -0300

    Aula 16/09

--- DOM/ex2.js
+++ DOM/ex2.js
@@ -3,9 +3,35 @@ let btnCalcular = document.getElementById('calcular');
 btnCalcular.addEventListener('click', function(evento){
 
     evento.preventDefault();
+    
+    let serv = document.getElementById('servico');
+    let prazo = document.getElementById('prazo');
+    let res = document.getElementById('resultado')
+
+    /*recupera os dados da matriz*/
+    let preco = calculaPreco( serv.value, prazo.value);
+
+    res.innerText = ` O preco é ${preco}`;
+
+
+    console.log(serv.value);
+  
+    //ver dados do evento (click) no console
+
     console.log(evento);
 
 });
+
+    function calculaPreco( servico, prazo){
+
+        let tabela = [['$$$', '$$', '$'],
+                    ['$$$$', '$$$', '$$'],
+                    ['Não faz', 'Não faz', '$$$']];
+
+            return tabela[servico][prazo];
+    }
+
+
 
 
 /*
